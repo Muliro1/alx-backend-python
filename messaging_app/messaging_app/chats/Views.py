eaab0de461d8aaa9d3b9bf3d6eb68e5dde3a9d15
Muliro1 <mulirokhaemba@gmail.com>
2025-06-06 15:58:16 +0300

    fix

--- messaging_app/messaging_app/chats/Views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from chats.models import Message
-from chats.serializers import MessageSerializer
-from chats.permissions import IsParticipantOfConversation
-
-class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
-    permission_classes = [IsParticipantOfConversation]
