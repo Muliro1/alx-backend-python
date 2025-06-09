b605eb3f29338da2a12f0d7b4240514cfa8ae571
Muliro1 <mulirokhaemba@gmail.com>
2025-06-09 23:00:16 +0300

    ft-edited-by

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -11,6 +11,13 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
+    edited_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='edited_messages'
+    )
 
     def __str__(self):
         return f"From {self.sender} to {self.receiver}: {self.content[:20]}"
